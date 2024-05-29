c1bc8f856e184eeb874b716c88d36253374a301d
Alfred-Lau <jian.liu@icloud.com>
2024-05-29 23:16:51 +0800

    feat: 支持 utm 归因

--- packages/utils/src/query.ts
+++ packages/utils/src/query.ts
@@ -1,6 +1,6 @@
-import qs from 'qs';
+import qs from "qs";
 
-export function parseObjToQuery(data: object): string {
+export function parseObjToQuery(data: any): string {
   const result = [];
   for (const key in data) {
     if (Object.prototype.hasOwnProperty.call(data, key)) {
